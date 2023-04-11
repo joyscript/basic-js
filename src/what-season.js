3e2d191072ecc19c236f45e52e443b5669f11a24
joyscript <joyscript7@gmail.com>
2023-04-11 07:42:21 +0300

    feat: solve "what-season"

--- src/what-season.js
+++ src/what-season.js
@@ -2,20 +2,28 @@ const { NotImplementedError } = require('../extensions/index.js');
 
 /**
  * Extract season from given date and expose the enemy scout!
- * 
+ *
  * @param {Date | FakeDate} date real or fake date
  * @returns {String} time of the year
- * 
+ *
  * @example
- * 
+ *
  * getSeason(new Date(2020, 02, 31)) => 'spring'
- * 
+ *
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSeason(date) {
+  if (!date) return 'Unable to determine the time of year!';
+
+  try {
+    date.getTime();
+  } catch {
+    throw new Error('Invalid date!');
+  }
+
+  let seasons = ['winter', 'spring', 'summer', 'autumn', 'winter'];
+  return seasons[Math.floor((date.getMonth() + 1) / 3)];
 }
 
 module.exports = {
-  getSeason
+  getSeason,
 };
