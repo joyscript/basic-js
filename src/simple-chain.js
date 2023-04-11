c8a8c611a8bc196cadf171d0e7c1f0d4f14db46f
joyscript <joyscript7@gmail.com>
2023-04-11 23:56:40 +0300

    feat: solve "simple-chain"

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -2,31 +2,36 @@ const { NotImplementedError } = require('../extensions/index.js');
 
 /**
  * Implement chainMaker object according to task description
- * 
+ *
  */
 const chainMaker = {
+  chain: [],
   getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.chain.split('~~').length;
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  addLink(value) {
+    this.chain.push(`( ${value} )`);
+    return this;
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  removeLink(position) {
+    if (!this.chain[position - 1]) {
+      this.chain.length = 0;
+      throw new Error("You can't remove incorrect link!");
+    }
+    this.chain.splice(position - 1, 1);
+    return this;
   },
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    this.chain.reverse();
+    return this;
   },
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    let res = this.chain.join('~~');
+    this.chain.length = 0;
+    return res;
+  },
 };
 
 module.exports = {
-  chainMaker
+  chainMaker,
 };
